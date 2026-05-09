37ae7dddf9a075638340c4a0b678d7e036cd40e5
Steve Ward <148103309+crazysteeeb@users.noreply.github.com>
2026-05-08 21:58:01 -0500

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+    // Unset all session variables
+    session_unset();
+    // Destroy the session
+    session_destroy();
+    // Remove the session cookie (if it exists)
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000, 
+            $params["path"], 
+            $params["domain"], 
+            $params["secure"], 
+            $params["httponly"]
+        );
+    }
+    session_write_close();
+    // Redirect to the home page
+    header('Location: /');
+    exit();
+?>
